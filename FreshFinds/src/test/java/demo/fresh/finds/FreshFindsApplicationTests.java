5fe7af89d7ae2241d98f6d4a8c731595194675cb
Alrich Erasmus <alricherasmus123@gmail.com>
2024-06-20 21:28:09 +0200

    initial commit

---
+++ FreshFinds/src/test/java/demo/fresh/finds/FreshFindsApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.fresh.finds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshFindsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
